116ab0c22c3bd934eacc1906c5f87313ebf9796a
Liza-Cheptsova <cheptsova.liza@mail.ru>
2021-07-18 13:40:31 +0300

    Updates

--- precache-manifest.29ba39bc8ff5fc4e414d041a93f3b1dc.js
+++ precache-manifest.29ba39bc8ff5fc4e414d041a93f3b1dc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "946500d6c83cd2d76e248eb213537a63",
+    "revision": "ff758491e98667a427383ec90c6bf031",
     "url": "/todolist/index.html"
   },
   {
-    "revision": "92e19ca405b6e40856a6",
+    "revision": "df03eebaa3ef58b5f034",
     "url": "/todolist/static/css/main.12d83c41.chunk.css"
   },
   {
-    "revision": "78234d0a153240a4c99b",
-    "url": "/todolist/static/js/2.2b4331a2.chunk.js"
+    "revision": "ff37b77723f4710bbd32",
+    "url": "/todolist/static/js/2.b3a030e0.chunk.js"
   },
   {
     "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
-    "url": "/todolist/static/js/2.2b4331a2.chunk.js.LICENSE.txt"
+    "url": "/todolist/static/js/2.b3a030e0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "92e19ca405b6e40856a6",
-    "url": "/todolist/static/js/main.7833fcc1.chunk.js"
+    "revision": "df03eebaa3ef58b5f034",
+    "url": "/todolist/static/js/main.33691a62.chunk.js"
   },
   {
     "revision": "471e54e937a3aef18e16",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todolist/precache-manifest.df061539cf5b4918898f843a07579476.js"
+  "/todolist/precache-manifest.29ba39bc8ff5fc4e414d041a93f3b1dc.js"
 );
 
 self.addEventListener('message', (event) => {
