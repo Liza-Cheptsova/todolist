fcac90f407f37b856bf41baebade009834c61836
Liza-Cheptsova <cheptsova.liza@mail.ru>
2021-07-18 12:34:29 +0300

    Updates

---
+++ precache-manifest.7ce2e90ba7fb755282c5d05839d52004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d5d7f8242fb2ac1c957a3ce046ea5f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "86f16bbc98386062bdd0",
+    "url": "/todolist/static/css/main.a5905b15.chunk.css"
+  },
+  {
+    "revision": "fa4597208064eb097586",
+    "url": "/todolist/static/js/2.f5a6e41c.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.f5a6e41c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f16bbc98386062bdd0",
+    "url": "/todolist/static/js/main.788396ad.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
